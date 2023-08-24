2aeb9284f4d141f21460863f347350adea144789
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-08-24 12:11:57 +0530

    refactor: add Note#toSavedNoteEntity extension

--- shared/src/commonMain/kotlin/com/example/notes/domain/Note.kt
+++ shared/src/commonMain/kotlin/com/example/notes/domain/Note.kt
@@ -7,7 +7,7 @@ data class Note(
     val id: Long,
     val title: String,
     val content: String,
-    val isDeleted:Boolean
+    val isDeleted: Boolean
 )
 
 fun SavedNoteEntity.toNote(): Note {
@@ -16,6 +16,15 @@ fun SavedNoteEntity.toNote(): Note {
         title = this.title,
         content = this.content,
         isDeleted = this.isDeleted == 1L
+    )
+}
+
+fun Note.toSavedNoteEntity(): SavedNoteEntity {
+    return SavedNoteEntity(
+        id = this.id,
+        title = this.title,
+        content = this.content,
+        isDeleted = if (this.isDeleted) 1 else 0
     )
 }
 
