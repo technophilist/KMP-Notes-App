059a583428e95ace6c5046492b1345f3feb61af8
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-08-24 12:24:39 +0530

    refactor: add DefaultNotestRepository (impl of NotesRepository)

---
+++ shared/src/commonMain/kotlin/com/example/notes/data/DefaultNotesRepository.kt
@@ -0,0 +1,30 @@
+package com.example.notes.data
+
+import com.example.notes.data.local.localnotesdatasource.LocalNotesDataSource
+import com.example.notes.domain.Note
+import com.example.notes.domain.toNote
+import com.example.notes.domain.toSavedNoteEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class DefaultNotesRepository(
+    private val localNotesDataSource: LocalNotesDataSource
+) : NotesRepository {
+
+    override val savedNotesStream: Flow<List<Note>> =
+        localNotesDataSource.savedNotesStream.map { savedNoteEntities ->
+            savedNoteEntities.map { savedNoteEntity -> savedNoteEntity.toNote() }
+        }
+
+    override suspend fun saveNote(note: Note) {
+        localNotesDataSource.saveNote(note.toSavedNoteEntity())
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        localNotesDataSource.markNoteAsDeleted(note.toSavedNoteEntity().id)
+    }
+
+    override suspend fun deleteAllNotes() {
+        localNotesDataSource.deleteAllNotes()
+    }
+}
