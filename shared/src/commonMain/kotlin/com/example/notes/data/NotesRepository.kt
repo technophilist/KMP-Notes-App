4fbe041533af0bfc0a5d32732fc37d934f5e45e6
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-08-24 12:24:20 +0530

    refactor: add NotesRepository interface

---
+++ shared/src/commonMain/kotlin/com/example/notes/data/NotesRepository.kt
@@ -0,0 +1,28 @@
+package com.example.notes.data
+
+import com.example.notes.domain.Note
+import kotlinx.coroutines.flow.Flow
+
+interface NotesRepository {
+
+    /**
+     * A [Flow] of all saved notes.
+     */
+    val savedNotesStream: Flow<List<Note>>
+
+    /**
+     * Used to save a [Note].
+     */
+    suspend fun saveNote(note: Note)
+
+    /**
+     * Used to delete a [Note].
+     */
+    suspend fun deleteNote(note: Note)
+
+    /**
+     * Used to delete all saved [Note]s.
+     */
+    suspend fun deleteAllNotes()
+
+}
