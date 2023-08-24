99c6eacd21c180d47cca3082b159a450468b7373
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-08-24 09:28:18 +0530

    refactor: add DefaultLocalNotesDataSource (impl of LocalNotesDataSource)

---
+++ shared/src/commonMain/kotlin/com/example/notes/data/local/localnotesdatasource/DefaultLocalNotesDataSource.kt
@@ -0,0 +1,45 @@
+package com.example.notes.data.local.localnotesdatasource
+
+import app.cash.sqldelight.coroutines.asFlow
+import app.cash.sqldelight.coroutines.mapToList
+import com.example.notes.database.NotesDatabase
+import com.example.notes.database.SavedNoteEntity
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+
+class DefaultLocalNotesDataSource(
+    database: NotesDatabase,
+    private val ioDispatcher: CoroutineDispatcher
+) : LocalNotesDataSource {
+    private val queries = database.notesDatabaseQueries
+
+    override val savedNotesStream: Flow<List<SavedNoteEntity>> = queries.getAllSavedNotes()
+        .asFlow()
+        .mapToList(ioDispatcher)
+
+    override suspend fun saveNote(noteEntity: SavedNoteEntity) = withContext(ioDispatcher) {
+        queries.insertNote(
+            id = noteEntity.id,
+            title = noteEntity.title,
+            content = noteEntity.content,
+            isDeleted = noteEntity.isDeleted
+        )
+    }
+
+    override suspend fun permanentlyDeleteNoteWithId(id: Long) = withContext(ioDispatcher) {
+        queries.deleteNote(id)
+    }
+
+    override suspend fun markNoteAsDeleted(id: Long) = withContext(ioDispatcher) {
+        queries.markNoteAsDeleted(id)
+    }
+
+    override suspend fun markNoteAsNotDeleted(id: Long) = withContext(ioDispatcher) {
+        queries.markNoteAsNotDeleted(id)
+    }
+
+    override suspend fun deleteAllNotes() = withContext(ioDispatcher) {
+        queries.deleteAllNotes()
+    }
+}
