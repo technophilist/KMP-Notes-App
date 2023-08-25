b827c188791fea07d55e3de418fc1d2e7002cc4a
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-08-25 09:27:44 +0530

    fix: set compile sdk to 34 tp fix compilation errors

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.notes.android"
-    compileSdk = 33
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.example.notes.android"
         minSdk = 24
