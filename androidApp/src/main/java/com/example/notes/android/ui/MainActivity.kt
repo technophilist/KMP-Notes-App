4c38739dfe7d0166565208759b543c4985dc9be6
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-09-06 08:00:22 -0400

    fix: make app content go edge-to-edge

--- androidApp/src/main/java/com/example/notes/android/ui/MainActivity.kt
+++ androidApp/src/main/java/com/example/notes/android/ui/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.example.notes.Greeting
 import com.example.notes.android.NotesAppApplication
 import com.example.notes.android.ui.navigation.NotesAppNavigation
@@ -17,6 +18,9 @@ import com.example.notes.android.ui.theme.NotesAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // make the app edge-to-edge
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         val application = this.application as NotesAppApplication
         val appModule = application.appModule
 
