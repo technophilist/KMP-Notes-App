8f4d2d56f9b7d7346aeb259fd668ce3f267fc1ef
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-09-06 07:39:50 -0400

    refactor: call NotesAppNavigation composable in MainActivity

--- androidApp/src/main/java/com/example/notes/android/ui/MainActivity.kt
+++ androidApp/src/main/java/com/example/notes/android/ui/MainActivity.kt
@@ -10,30 +10,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.notes.Greeting
+import com.example.notes.android.NotesAppApplication
+import com.example.notes.android.ui.navigation.NotesAppNavigation
 import com.example.notes.android.ui.theme.NotesAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val application = this.application as NotesAppApplication
+        val appModule = application.appModule
+
         setContent {
             NotesAppTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    GreetingView(Greeting().greet())
-                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    content = { NotesAppNavigation(appModule = appModule) }
+                )
             }
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    NotesAppTheme {
-        GreetingView("Hello, Android!")
-    }
-}
