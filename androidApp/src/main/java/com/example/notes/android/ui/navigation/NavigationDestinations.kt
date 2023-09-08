88ea8e48ba3d0c5e9753980373f07857771a8463
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-09-07 20:04:15 -0400

    refactor: add NoteDetailScreen obj to NavigationDestinations sealed hierarchy

--- androidApp/src/main/java/com/example/notes/android/ui/navigation/NavigationDestinations.kt
+++ androidApp/src/main/java/com/example/notes/android/ui/navigation/NavigationDestinations.kt
@@ -2,4 +2,12 @@ package com.example.notes.android.ui.navigation
 
 sealed class NavigationDestinations(val route: String) {
     object HomeScreen : NavigationDestinations(route = "notes_app_home_screen")
+    object NoteDetailScreen :
+        NavigationDestinations(route = "notes_app_note_detail_screen/{noteId}") {
+        const val NAV_ARG_NOTE_ID = "noteId"
+        fun buildRoute(noteId: Long): String {
+            return "notes_app_note_detail_screen/$noteId"
+        }
+    }
+    
 }
