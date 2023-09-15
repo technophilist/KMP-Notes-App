620f42c586b231a9ae53a7f5bf8b26628c2b8ffb
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-09-15 17:23:08 -0400

    refactor: add AndroidHomeViewModel#deleteNote

--- androidApp/src/main/java/com/example/notes/android/ui/home/AndroidHomeViewModel.kt
+++ androidApp/src/main/java/com/example/notes/android/ui/home/AndroidHomeViewModel.kt
@@ -3,6 +3,7 @@ package com.example.notes.android.ui.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.notes.data.NotesRepository
+import com.example.notes.domain.Note
 import com.example.notes.ui.home.HomeViewModel
 
 class AndroidHomeViewModel(notesRepository: NotesRepository) : ViewModel() {
@@ -14,4 +15,5 @@ class AndroidHomeViewModel(notesRepository: NotesRepository) : ViewModel() {
 
     val uiState = viewModel.uiState
     fun search(searchText: String) = viewModel.search(searchText)
+    fun deleteNote(note: Note) = viewModel.deleteNote(note)
 }
