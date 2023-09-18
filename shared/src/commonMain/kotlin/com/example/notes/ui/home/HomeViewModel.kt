7aa3fe17f59e6ed0e94d477c54db6a71f7c18bd6
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-09-18 15:55:51 -0400

    fix: debounce before filtering notes when the search text changes

--- shared/src/commonMain/kotlin/com/example/notes/ui/home/HomeViewModel.kt
+++ shared/src/commonMain/kotlin/com/example/notes/ui/home/HomeViewModel.kt
@@ -6,6 +6,8 @@ import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
@@ -14,7 +16,7 @@ import kotlinx.coroutines.launch
 class HomeViewModel(
     private val notesRepository: NotesRepository,
     private val viewModelScope: CoroutineScope,
-    private val defaultDispatcher: CoroutineDispatcher
+    defaultDispatcher: CoroutineDispatcher
 ) {
 
     /**
@@ -23,27 +25,26 @@ class HomeViewModel(
     private val _uiState = MutableStateFlow(HomeScreenUiState(isLoadingSavedNotes = true))
     val uiState = _uiState as StateFlow<HomeScreenUiState>
 
+    private val currentSearchText = MutableStateFlow("")
+
     init {
         notesRepository.savedNotesStream.onEach { savedNotesList ->
             _uiState.update { it.copy(isLoadingSavedNotes = false, savedNotes = savedNotesList) }
         }.launchIn(viewModelScope)
-    }
 
-    /**
-     * Searches for notes with titles or contents containing the given [searchText].
-     */
-    fun search(searchText: String) {
-        viewModelScope.launch {
+        currentSearchText.debounce(200).onEach { searchText ->
             _uiState.update { it.copy(isLoadingSearchResults = true) }
             val savedNotes = _uiState.value.savedNotes
             // filtering notes with titles containing the search text
             val notesWithTitleContainingSearchText = savedNotes.filter {
                 it.title.contains(searchText, ignoreCase = true)
             }
-            _uiState.update {
-                it.copy(searchResults = notesWithTitleContainingSearchText)
-            }
+            _uiState.update { it.copy(searchResults = notesWithTitleContainingSearchText) }
+        }.flowOn(defaultDispatcher)
+            .launchIn(viewModelScope)
 
+        currentSearchText.debounce(200).onEach { searchText ->
+            val savedNotes = _uiState.value.savedNotes
             // filtering notes with the content containing the search text
             // since this is slower than the previous filtering operation above,
             // update ui state independently
@@ -56,7 +57,15 @@ class HomeViewModel(
             _uiState.update {
                 it.copy(isLoadingSearchResults = false)
             }
-        }
+        }.flowOn(defaultDispatcher)
+            .launchIn(viewModelScope)
+    }
+
+    /**
+     * Searches for notes with titles or contents containing the given [searchText].
+     */
+    fun search(searchText: String) {
+        currentSearchText.value = searchText
     }
 
     fun deleteNote(note: Note) {
