86e711a7c716efbd65b330c4fd20e61b50b9f806
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-09-19 18:50:10 -0400

    refactor: provide work manager configuration in Application class

--- androidApp/src/main/java/com/example/notes/android/NotesAppApplication.kt
+++ androidApp/src/main/java/com/example/notes/android/NotesAppApplication.kt
@@ -1,9 +1,15 @@
 package com.example.notes.android
 
 import android.app.Application
+import androidx.work.Configuration
+import com.example.notes.android.ui.workers.CleanupWorkerFactory
 import com.example.notes.di.AppModule
 
-class NotesAppApplication : Application() {
+class NotesAppApplication : Application(), Configuration.Provider {
 
     val appModule by lazy { AppModule(this) }
+
+    override fun getWorkManagerConfiguration(): Configuration = Configuration.Builder()
+        .setWorkerFactory(CleanupWorkerFactory(notesRepository = appModule.provideNotesRepository()))
+        .build()
 }
