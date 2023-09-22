f031970ba57e9b15454f942156af9188a43c29cf
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-09-21 22:55:08 -0400

    fix: bump kotlin gradle plugin to 1.9.10
    Older versions of kotlin will not work with
    Xcode 15

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("8.0.0").apply(false)
     id("com.android.library").version("8.0.0").apply(false)
-    kotlin("android").version("1.8.21").apply(false)
+    kotlin("android").version("1.9.10").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
 }
 
