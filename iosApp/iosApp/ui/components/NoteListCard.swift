e5c2c5687af6975ea50a1b3ab72c521060a86c04
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-09-22 16:35:45 -0400

    refactor(ios): add NoteListCard view

---
+++ iosApp/iosApp/ui/components/NoteListCard.swift
@@ -0,0 +1,20 @@
+import Foundation
+import SwiftUI
+import shared
+
+/// A view that displays the information of a Note object.
+/// Some of the contents might get truncated if the content property of the Note object is too long.
+struct NoteListCard : View{
+    var note : Note
+    var body : some View{
+        VStack(alignment: .leading){
+            Text(note.title)
+                .font(.headline)
+                .fontWeight(.bold)
+                .lineLimit(2)
+            
+            Text(note.content)
+                .lineLimit(3)
+        }
+    }
+}
