a8498d27e265c381d5236b430834d7b8bfa545a3
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-09-26 08:26:08 -0400

    refactor(ios): add IOSHomeViewModel

--- iosApp/iosApp/ui/home/HomeViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  HomeViewModel.swift
-//  iosApp
-//
-//  Created by Marsh Royden on 25/09/23.
-//  Copyright © 2023 orgName. All rights reserved.
-//
-
-import Foundation
---
+++ iosApp/iosApp/ui/home/IOSHomeViewModel.swift
@@ -0,0 +1,44 @@
+import Foundation
+import shared
+
+
+
+class IOSHomeViewModel : ObservableObject {
+    
+    private var homeViewModel : HomeViewModel
+    private var collectHandle : Kotlinx_coroutines_coreDisposableHandle? = nil
+    
+    @Published var searchText = ""
+    @Published var uiState: HomeScreenUiState = HomeScreenUiState(
+        isLoadingSavedNotes: false,
+        isLoadingSearchResults: false,
+        savedNotes: [],
+        searchResults: []
+    )
+    
+    init(notesRepository : NotesRepository , dispatchersProvider : DispatchersProvider){
+        homeViewModel = HomeViewModel(
+            notesRepository: notesRepository,
+            coroutineScope: nil,
+            defaultDispatcher: dispatchersProvider.defaultDispatcher
+        )
+        collectHandle = homeViewModel.uiState.collect { newUiState in
+            if(newUiState != nil){
+                self.uiState = newUiState!
+            }
+        }
+    }
+    
+    func search(searchText: String) {
+        homeViewModel.search(searchText: searchText)
+    }
+    
+    func deleteNotes(indexSet:IndexSet) {
+        indexSet.forEach { homeViewModel.deleteNote(note: uiState.savedNotes[$0]) }
+    }
+    
+    func dispose(){
+        collectHandle?.dispose()
+    }
+}
+
