95a4c2404941314c20dba1fbef887556ec25914b
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-09-27 13:53:49 -0400

    refactor(ios): make NoteDetailScreen View make use of NoteDetailViewModel

--- iosApp/iosApp/ui/home/notedetail/NoteDetailScreen.swift
+++ iosApp/iosApp/ui/home/notedetail/NoteDetailScreen.swift
@@ -3,14 +3,23 @@ import shared
 import SwiftUI
 
 struct NoteDetailScreen : View {
-    @State var titleText = ""
-    @State var contentText = ""
+    @ObservedObject private var noteDetailViewModel:IOSNoteDetailViewModel
+    
+    init(appModule:AppModule) {
+        self.noteDetailViewModel = IOSNoteDetailViewModel(
+            notesRepository: appModule.provideNotesRepository(),
+            currentNoteId: nil
+        )
+    }
+    
     var body : some View {
         VStack(alignment: .center) {
-            TextField("Title",text:$contentText,axis: .vertical)
+            TextField("Title",text: $noteDetailViewModel.contentText,axis: .vertical)
+                .onChange(of: noteDetailViewModel.contentText){ noteDetailViewModel.onTitleChange($0) }
                 .font(.largeTitle.weight(.bold))
-
-            TextEditor(text: $titleText)
+            
+            TextEditor(text: $noteDetailViewModel.contentText)
+                .onChange(of: noteDetailViewModel.contentText){ noteDetailViewModel.onContentChange($0) }
                 .font(.headline)
             
         }.frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .topLeading)
