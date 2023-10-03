4e90e851af0e73049d58322f80877f16e38ed2f5
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-10-03 09:18:04 -0400

    fix: fix status bar colors in light theme on android

--- androidApp/src/main/java/com/example/notes/android/ui/MainActivity.kt
+++ androidApp/src/main/java/com/example/notes/android/ui/MainActivity.kt
@@ -3,23 +3,27 @@ package com.example.notes.android.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
+import android.content.res.Configuration
 import com.example.notes.android.NotesAppApplication
 import com.example.notes.android.ui.navigation.NotesAppNavigation
 import com.example.notes.android.ui.theme.NotesAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // make the app edge-to-edge
         WindowCompat.setDecorFitsSystemWindows(window, false)
-
+        // set appropriate status bar colors so that the items in the status bar are legible
+        setStatusBarColor()
         val application = this.application as NotesAppApplication
         val appModule = application.appModule
-
         setContent {
             NotesAppTheme {
                 Surface(
@@ -28,6 +32,12 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+    }
+
+    private fun setStatusBarColor() {
+        val controller = ViewCompat.getWindowInsetsController(window.decorView)
+        val isDarkThemeActive = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+        controller?.isAppearanceLightStatusBars = !isDarkThemeActive
     }
 }
 
